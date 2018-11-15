f4d13f9bcca7d1b7c739352f20977a1f490d63df
Hystee <hysteebyhystee@gmail.com>
2018-11-15 16:48:27 -0500

    feat(video): add documentation

--- imageprocessing/creategifs.py
+++ imageprocessing/creategifs.py
@@ -1,23 +1,19 @@
 from moviepy.editor import *
 
 def stackIt(path):
-  clip = VideoFileClip(path)
+  clip = VideoFileClip(path) # path is relative to where the file is being run
   length = clip.duration
   step = length / 4
   clip0 = clip.subclip(0,1)
   clip1 = clip.subclip(step,1+step)
-  clip2 = clip.subclip(step+step,1+step+step)
-  clip3 = clip.subclip(step+step+step,1+step+step+step)
+  clip2 = clip.subclip(2*step,1+2*step)
+  clip3 = clip.subclip(3*step,1+3*step)
   final_clip = clips_array([[clip0, clip1],[clip2, clip3]])
   return final_clip.resize(width=480)
 
-  # .write_gif("steppedstack.gif",12)
-
 def gifIt(clip, outputPath):
-  clip.write_gif(outputPath, 12)
+  clip.write_gif(outputPath, 12) # path is relative to where to file is being run
 
-# final = stackIt("trailer_1080p.mov")
-# gifIt(final, "functional.gif")
-
-# different = stackIt("IMG_1064.mov")
-# gifIt(different, "test.gif")
+# example
+# different = stackIt('src/ComicBoom_Testing.mov')
+# gifIt(different, '../repotest.gif')
